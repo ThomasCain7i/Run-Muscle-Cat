ec8182072dcfb6296628117e18c3af426258772c
ThomasCain7i <115077729+ThomasCain7i@users.noreply.github.com>
2024-01-29 22:43:01 +0000

    Timer and Timer UI done

--- Assets/Kyree/Scripts/Timer.cs
+++ Assets/Kyree/Scripts/Timer.cs
@@ -6,40 +6,41 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    public float timeRemaining = 0f;
-    public bool timeIsRunning = false;
+    public float timeRemaining = 60f; // Set the initial time here
+    public bool timeIsRunning = true; // Change to true if you want the timer to start running immediately
     public TMP_Text timeText;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Make a condition using the start point script or collision
-        timeIsRunning = false;
+        // Make a condition using the start point script or collision
+        // If you want the timer to start immediately, uncomment the line below
+        // timeIsRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (timeIsRunning)
         {
-            if (timeRemaining >= 0)
+            if (timeRemaining > 0)
             {
-                timeRemaining += Time.deltaTime;
+                timeRemaining -= Time.deltaTime;
                 DisplayTime(timeRemaining);
             }
+            else
+            {
+                // Time has reached zero, you might want to perform some action here
+                timeIsRunning = false;
+            }
         }
-
     }
 
     void DisplayTime(float timeToDisplay)
     {
-        timeToDisplay += 1;
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-        timeText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
-
-
 }
 
